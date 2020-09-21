c17e1e2dcbd7a6d1ad27ea5d02dbaa2f98dc0e9a
Bruno Correia <oliveirb@coventry.ac.uk>
2020-09-21 16:37:12 +0000

    Installed React boilerplate app.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   
+        <p>304CEM</p>
+      
+    </div>
+  );
+}
+
+export default App;
